d00e4b68085cbe872fa0f22b97adbfdb79a1806f
test <test@gmail.com>
2023-06-30 17:16:25 -0600

    +

---
+++ target/classes/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO empleado (id, nombre, cargo, salario) VALUES (1, 'Alice', 'Developer', 60000.00);
+INSERT INTO empleado (id, nombre, cargo, salario) VALUES (2, 'Bob', 'Manager', 80000.00);
+INSERT INTO empleado (id, nombre, cargo, salario) VALUES (3, 'Charlie', 'Designer', 70000.00);
+INSERT INTO empleado (id, nombre, cargo, salario) VALUES (4, 'David', 'Developer', 60000.00);
+INSERT INTO empleado (id, nombre, cargo, salario) VALUES (5, 'Eve', 'Product Manager', 90000.00);
